6d59cae148c1e592dd09b8ddb1f326306e633ad3
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-09-02 20:19:45 +0400

    main cpp

--- labs/1/SimUDuck1/SimUDuck1/main.cpp
+++ labs/1/SimUDuck1/SimUDuck1/main.cpp
@@ -1,7 +1,49 @@
 ﻿#include "pch.h"
-#include <iostream>
+#include "MallardDuck.h"
+#include "RedheadDuck.h"
+#include "RubberDuck.h"
+#include "DecoyDuck.h"
+#include "FlyRocketPowered.h"
+
+using namespace std;
 
 int main()
 {
+	CMallardDuck mallardDuck;
+
+	mallardDuck.Display();
+	mallardDuck.PerformQuack();
+	mallardDuck.PerformFly();
+	mallardDuck.PerformDance();
+	mallardDuck.Swim();
+
+	mallardDuck.SetFlyBehavior(make_unique<CFlyRocketPowered>());
+
+	mallardDuck.PerformFly();
+
+	CRedheadDuck redheadDuck;
+
+	redheadDuck.Display();
+	redheadDuck.PerformQuack();
+	redheadDuck.PerformFly();
+	redheadDuck.PerformDance();
+	redheadDuck.Swim();
+
+	CRubberDuck rubberDuck;
+
+	rubberDuck.Display();
+	rubberDuck.PerformQuack();
+	rubberDuck.PerformFly();
+	rubberDuck.PerformDance();
+	rubberDuck.Swim();
+
+	CDecoyDuck decoyDuck;
+
+	decoyDuck.Display();
+	decoyDuck.PerformQuack();
+	decoyDuck.PerformFly();
+	decoyDuck.PerformDance();
+	decoyDuck.Swim();
+
 	return 0;
 }
