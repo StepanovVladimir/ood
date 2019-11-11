43f6ba81f5202625f70f61c8dbff97a86b3dccae
StepanovVladimir <33180536+StepanovVladimir@users.noreply.github.com>
2019-11-11 09:37:43 +0300

    Update tests.cpp

--- labs/6/ClassAdapter/tests/tests.cpp
+++ labs/6/ClassAdapter/tests/tests.cpp
@@ -7,18 +7,13 @@ using namespace std;
 using namespace app;
 using namespace modern_graphics_lib;
 
-TEST_CASE("Adapter move to and line to tests")
+TEST_CASE("Draw line from default start position")
 {
 	ostringstream outStrm;
 	CGraphicsAdapter adapter(outStrm);
 
 	adapter.BeginDraw();
 	adapter.LineTo(10, 15);
-	adapter.SetColor(0x00ff00ff);
-	adapter.MoveTo(20, 25);
-	adapter.LineTo(30, 35);
-	adapter.SetColor(0xff00ff00);
-	adapter.LineTo(40, 45);
 	adapter.EndDraw();
 
 	istringstream result(outStrm.str());
@@ -33,9 +28,59 @@ TEST_CASE("Adapter move to and line to tests")
 	getline(result, str);
 	CHECK(str == "  </line>");
 	getline(result, str);
+	CHECK(str == "</draw>");
+
+	CHECK_FALSE(getline(result, str));
+}
+
+TEST_CASE("Adapter set color, move to and line to tests")
+{
+	ostringstream outStrm;
+	CGraphicsAdapter adapter(outStrm);
+
+	adapter.BeginDraw();
+	adapter.SetColor(0x00ff00ff);
+	adapter.MoveTo(20, 25);
+	adapter.LineTo(30, 35);
+	adapter.EndDraw();
+
+	istringstream result(outStrm.str());
+	string str;
+
+	getline(result, str);
+	CHECK(str == "<draw>");
+	getline(result, str);
 	CHECK(str == "  <line fromX=\"20\" fromY=\"25\" toX=\"30\" toY=\"35\">");
 	getline(result, str);
 	CHECK(str == "    <color r=\"1\" g=\"0\" b=\"1\" a=\"0\"/>");
+	getline(result, str);
+	CHECK(str == "  </line>");
+	getline(result, str);
+	CHECK(str == "</draw>");
+
+	CHECK_FALSE(getline(result, str));
+}
+
+TEST_CASE("Adapter twice line to tests")
+{
+	ostringstream outStrm;
+	CGraphicsAdapter adapter(outStrm);
+
+	adapter.BeginDraw();
+	adapter.SetColor(0xff00ff00);
+	adapter.LineTo(30, 35);
+	adapter.LineTo(40, 45);
+	adapter.EndDraw();
+
+	istringstream result(outStrm.str());
+	string str;
+
+	getline(result, str);
+	CHECK(str == "<draw>");
+	getline(result, str);
+	CHECK(str == "  <line fromX=\"0\" fromY=\"0\" toX=\"30\" toY=\"35\">");
+	getline(result, str);
+	CHECK(str == "    <color r=\"0\" g=\"1\" b=\"0\" a=\"1\"/>");
 	getline(result, str);
 	CHECK(str == "  </line>");
 	getline(result, str);
